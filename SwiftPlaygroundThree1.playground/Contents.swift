b878b949dea1e96ac0a0e933536b6919ff22600c
Cheyna Hess <cheynah@hawaii.edu>
2022-02-19 12:03:02 -1000

    Completed parts 1-8

--- SwiftPlaygroundThree1.playground/Contents.swift
+++ SwiftPlaygroundThree1.playground/Contents.swift
@@ -1,3 +1,26 @@
 import UIKit
 
 var greeting = "Hello, playground"
+//Part 3
+var fruitNames: [String]
+//var numbers: [Int]
+
+//part 4
+fruitNames = ["Kiwi", "Apples", "Watermelon"]
+
+//Part 5
+print(fruitNames)
+
+//Part 6
+print("I like to eat\(fruitNames)")
+
+
+//Part 7: Iterate through all items in the array using the "for-in" syntax
+for name in fruitNames{
+    print("I like to eat " + name)
+}
+
+//Part 8
+print(fruitNames[1])
+
+//Need to conmplete Part 9 on your own
